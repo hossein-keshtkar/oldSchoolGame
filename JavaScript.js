f0c89c005bfd7ddfcae5d6eb537ef3ffe0355dec
Hossein Keshtkar Haranaki <rock.hkh.g1@gmail.com>
2021-08-07 22:14:34 +0430

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,63 +1,64 @@
   
-
 let random;
 let point;
 let max;
 let record = [];
-point = parseInt(document.getElementById('score__point').innerHTML);
 
+
+ // Don't repeat yourself (DRY coding) - refactoring the code
+function colorChanger(id,color){
+  document.getElementById(id).style.color= color;
+};
+function displayMessage(id,message){
+  document.getElementById(id).innerHTML= message;
+};
+function randomMaker(){
+  random = Math.trunc(Math.random() * 20 + 1);
+  return random;
+};
+
+point = parseInt(document.getElementById('score__point').innerHTML);
   document.getElementById('btn__again').addEventListener('click', function() {
-    random = Math.trunc(Math.random() *20 + 1);
+    randomMaker();
     document.body.style.background = 'rgb(35, 35, 48)';
-    document.getElementById('questionMark').style.color= 'rgb(35, 35, 48)';
-    document.getElementById('btn__again').style.color= 'rgb(35, 35, 48)';
-    document.getElementById('btn__check').style.color= 'rgb(35, 35, 48)';
     document.getElementById('inputBox').style.background= 'rgb(35, 35, 48)';
-    document.getElementById('questionMark').innerHTML= '?';
-    document.getElementById("message").innerHTML= "Start guessing...";
-    document.getElementById('score__point').innerHTML = 20;
+    colorChanger("questionMark","rgb(35, 35, 48)");
+    colorChanger("btn__again","rgb(35, 35, 48)");
+    colorChanger("btn__check","rgb(35, 35, 48)");
+    displayMessage("questionMark","?");
+    displayMessage("message","Start guessing...");
+    displayMessage("score__point","20");
     point = 20;
     return random;
-  })
+  });
 
   document.getElementById('btn__check').addEventListener('click', function() {
     let input = document.getElementById('inputBox').value;
     input = parseInt(input);
     if (input === random){
-      document.getElementById("message").innerHTML= "you won!🏆";
+
+      // Don't repeat yourself - refactoring the code above
+      displayMessage("message","you won!🏆");
       document.body.style.background='green';
-      document.getElementById('questionMark').style.color= 'green';
-      document.getElementById('btn__again').style.color= 'green';
-      document.getElementById('btn__check').style.color= 'green';
       document.getElementById('inputBox').style.background= 'green';
-      document.getElementById('questionMark').innerHTML= random;
+      colorChanger("questionMark", "green");
+      colorChanger("btn__again", "green");
+      colorChanger("btn__check", "green");
+      displayMessage("questionMark",random);
       record.push(point);
       max = parseInt(document.getElementById('max__point').innerHTML);
       max = Math.max(...record);
       document.getElementById('max__point').innerHTML = max;
     }
 
-
-    // } else if (input > random) {
-    //   document.getElementById("message").innerHTML= "Too High 🔺";
-    //   point = parseInt(document.getElementById('score__point').innerHTML);
-    //   point--;
-    //   document.getElementById('score__point').innerHTML = point;
-    // } else {
-    //   document.getElementById("message").innerHTML= "Too Low 🔻";
-    //   point = parseInt(document.getElementById('score__point').innerHTML);
-    //   point--;
-    //   document.getElementById('score__point').innerHTML = point;
-    // }
-
     // Don't repeat yourself - refactoring the code above
     else if (input !== random) {
-      document.getElementById("message").innerHTML = input > random ? "Too High 🔺" : "Too Low 🔻";
+      displayMessage("message", input > random ? "Too High 🔺" : "Too Low 🔻");
       point--;
-      document.getElementById('score__point').innerHTML = point;
+      displayMessage("score__point", point);
     }
     return message;
   });
-  random = Math.trunc(Math.random() * 20 + 1);
+  randomMaker();
 
   
